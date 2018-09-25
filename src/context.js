53498f5240431f41cbb74abbf4bc6f2bb1a82422
Ivan Garcia <ivanvlora@gmail.com>
2018-09-25 16:21:47 +0900

    initial commit

---
+++ src/context.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "DELETE_CONTACT":
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "122-122-6543"
+      },
+      {
+        id: 2,
+        name: "Karen Williams",
+        email: "kwill@gmail.com",
+        phone: "542-234-5489"
+      },
+      {
+        id: 3,
+        name: "Homer Simpson",
+        email: "hsimpson@gmail.com",
+        phone: "422-121-1243"
+      }
+    ],
+    dispatch: action => this.setState(state => reducer(state, action))
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
